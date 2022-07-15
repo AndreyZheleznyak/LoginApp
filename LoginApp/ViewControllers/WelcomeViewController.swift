4927e231ff3ddcf0c6c2c8272878cb9f059b9ea6
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2022-07-15 14:38:59 +0300

    Setup WelcomeViewController.

---
+++ LoginApp/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var user = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(user)!"
+    }
+
+}
